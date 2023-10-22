2549cabff35bcc6c1eab42bfb3179ae8752d4507
Andres Martinez <andrepipemc@gmail.com>
2023-10-21 21:13:07 -0500

    chore(deploy): add configuration deployment vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    plugins: ['prettier-plugin-astro'],
-    integrations: [tailwind()]
+  plugins: ['prettier-plugin-astro'],
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
